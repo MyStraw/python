e0cb85460f46b268c2eeb56591f18effaf7cac0c
MyStraw <kikongdosa@gmail.com>
2023-06-01 14:43:07 +0900

    first commit

---
+++ 06_01_첫수업.py
@@ -0,0 +1,30 @@
+
+x=10
+y=20        #주석은 샵으로
+x+y
+
+print(x+y)
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+print(fruits[1])
+fruits[1] = "orange"
+print(fruits)
+
+print()
+
+num = 10
+text = "apple"
+print(str(num) + text)
+
+print()
+
+num = 10
+float_num = float(num)
+str_num = str(num)
+bool_num = bool(num)
+
+print(type(num))         # <class 'int'>
+print(type(float_num))   # <class 'float'>
+print(type(str_num))     # <class 'str'>
+print(type(bool_num))    # <class 'bool'>

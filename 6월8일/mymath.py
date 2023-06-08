be4a338da93407480e862b9127641e030733d118
MyStraw <kikongdosa@gmail.com>
2023-06-08 17:44:59 +0900

    6월8일 수업끝

---
+++ 6월8일/mymath.py
@@ -0,0 +1,15 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y==0:
+        print("0으로 나누면 우짜노")
+    else:
+        return x / y
+
